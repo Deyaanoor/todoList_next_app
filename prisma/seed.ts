7f34663fe76ca1901e7b75d6975ef88e0c5f4724
Deyaanoor <127193661+Deyaanoor@users.noreply.github.com>
2025-07-30 01:02:01 +0300

    شرح موجز للتغييرات اللي عملتها

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import{faker}from "@faker-js/faker";
+
+import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 async function main() {
 
-    await prisma.user.createMany({
-    data: Array.from({ length: 10 }, () => ({
-      name: faker.internet.username(),
-      email: faker.internet.email(),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zip: faker.location.zipCode(),
-      }
-    }))
-  });
+
 }
 
 main()
